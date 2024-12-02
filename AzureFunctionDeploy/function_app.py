4ab87511e32d90afe32bbd3f737e054cb3251299
sandhya16-p <balusan16@gmail.com>
2024-12-02 10:13:45 +0530

    adding files

--- AzureFunctionDeploy/function_app.py
+++ AzureFunctionDeploy/function_app.py
@@ -1,10 +1,11 @@
 import azure.functions as func
 import logging
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ADMIN)
+
+@app.route(route="pythonfunction")
+def pythonfunction(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -18,6 +19,7 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
